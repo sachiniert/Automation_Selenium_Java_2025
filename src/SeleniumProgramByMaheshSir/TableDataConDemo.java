c65cf51749093874599912c5233f32613066565b
sachiniert <sachin.yadav.sdet@gmail.com>
2025-06-30 10:39:22 +0530

    Find first 5 value of the table

--- src/SeleniumProgramByMaheshSir/TableDataConDemo.java
+++ src/SeleniumProgramByMaheshSir/TableDataConDemo.java
@@ -1,9 +1,32 @@
 package SeleniumProgramByMaheshSir;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TableDataConDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		WebDriver	 driver= new ChromeDriver();
+	     driver.get("https://testingmasters.weebly.com/webtables.html");
+	 List<String> expectedAnlayst= new ArrayList<String>();
+
+	 expectedAnlayst.add("S148106");
+	 expectedAnlayst.add("S148107");
+	 expectedAnlayst.add("S148109");
+	 expectedAnlayst.add("S148111");
+
+	List<WebElement>allheaders=driver.findElements(By.xpath("//table[@id='VisitingTable']//tbody/tr/td[4][starts-with(text(),'demo')]/preceding-sibling::td[2]"));
+
+	List<String> actualAnalyst= new ArrayList<String>();
+	for(int i=0;i<=allheaders.size()-1;i++)
+	{
+
+		actualAnalyst.add(allheaders.get(i).getText());
+	}
+	System.out.println(expectedAnlayst.containsAll(actualAnalyst));
 
 	}
 
---
+++ src/SeleniumProgramByMaheshSir/TableFirststFiveGet.java
@@ -0,0 +1,27 @@
+package SeleniumProgramByMaheshSir;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TableFirststFiveGet {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		WebDriver	 driver= new ChromeDriver();
+	     driver.get("https://testingmasters.weebly.com/webtables.html");
+	     Thread.sleep(5000);
+
+	     List<WebElement>first6=driver.findElements(By.xpath("(//table[@id='VisitingTable']//tbody//tr/td[1])[position()<6]"));
+
+	     for(int i=0; i<=first6.size()-1; i++)
+	     {
+	    	 
+	    	 first6.get(i).click();
+	     }
+	}
+
+}
