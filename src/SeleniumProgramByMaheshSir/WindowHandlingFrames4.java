041c59f0b0cf2ae6cdd42c3092c3363e2f57c01b
sachiniert <sachin.yadav.sdet@gmail.com>
2025-07-01 10:29:37 +0530

    Frames handles in webpage one webpage mulltiple frames

---
+++ src/SeleniumProgramByMaheshSir/WindowHandlingFrames4.java
@@ -0,0 +1,24 @@
+package SeleniumProgramByMaheshSir;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WindowHandlingFrames4 {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		 WebDriver	 driver= new ChromeDriver();
+	     driver.get("https://demo.guru99.com/test/guru99home/");
+	     
+	     Thread.sleep(4000);
+	    // driver.switchTo().frame(1);
+	    WebElement frameid= driver.findElement(By.xpath("//iframe[@name='a077aa5e']"));
+	     driver.switchTo().frame(frameid);
+	     
+	     driver.findElement(By.xpath("//a/img[@src='Jmeter720.png']")).click();
+	     driver.switchTo().defaultContent();
+	}
+
+}
