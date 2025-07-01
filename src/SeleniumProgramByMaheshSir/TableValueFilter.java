c7e5f7efe1c8af18db935d1dcab3a0d2db1a924f
sachiniert <sachin.yadav.sdet@gmail.com>
2025-07-01 09:12:53 +0530

    This program not working due to xpath and click button after 1 july i will check this issue

---
+++ src/SeleniumProgramByMaheshSir/TableValueFilter.java
@@ -0,0 +1,22 @@
+package SeleniumProgramByMaheshSir;
+import java.util.List;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class TableValueFilter {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		WebDriver	 driver= new ChromeDriver();
+	     driver.get("https://testingmasters.weebly.com/webtables.html");
+	Thread.sleep(4000);
+
+	List<WebElement>allcheck=driver.findElements(By.xpath("//table[@id='VisitingTable']//tbody//tr/td[1]"));
+
+
+	for(int i=allcheck.size();i>= allcheck.size()-4   ;i--)
+	{
+		driver.findElement(By.xpath("(//table[@id='VisitingTable']//tbody//tr/td[1])[position()=" +i+ "]")).click();
+	}
+	}
+}
