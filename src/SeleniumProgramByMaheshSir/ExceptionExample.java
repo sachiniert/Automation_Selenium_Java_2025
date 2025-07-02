d54432996605ce8cb631bd99c0411a45554c7671
sachiniert <sachin.yadav.sdet@gmail.com>
2025-07-02 09:38:28 +0530

    Exception method use here and resolve issue

---
+++ src/SeleniumProgramByMaheshSir/ExceptionExample.java
@@ -0,0 +1,39 @@
+package SeleniumProgramByMaheshSir;
+import java.util.List;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ExceptionExample {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://demo.guru99.com/test/guru99home/");
+		
+		List<WebElement> allframes = driver.findElements(By.tagName("iframe"));
+		
+		System.out.println(allframes.size());
+		Thread.sleep(4000);
+		
+		for(int i=0; i<=allframes.size()-1;i++)
+		{
+			driver.switchTo().frame(i);
+
+			try
+			{
+				WebElement link = driver.findElement(By.xpath("//a/img[@src='Jmeter720.png']"));
+				if(link.isEnabled())
+				{
+					link.click();
+				}
+			}
+			catch(Exception E)
+			{
+				driver.switchTo().defaultContent();
+			}
+			
+		}
+
+	}
+
+}
--- src/SeleniumProgramByMaheshSir/WindowHandler2.java
+++ src/SeleniumProgramByMaheshSir/WindowHandler2.java
@@ -1,9 +1,42 @@
 package SeleniumProgramByMaheshSir;
 
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class WindowHandler2 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
+		WebDriver	 driver= new ChromeDriver();
+	     driver.get("https://naukri.com/");
+	     
+	     String parentWindow = driver.getWindowHandle();
+	     System.out.println(parentWindow);
+	     
+	    // driver.findElement(By.xpath("//span[@title='Remote']")).click();
+	     Set<String> allwindow = driver.getWindowHandles();
+	     System.out.println(allwindow);//nak,cts,capg
+	     
+	     for(String currentwindow:allwindow)
+	     {
+	    	 
+	    	 if(!currentwindow.equals(allwindow))
+	    	 {
+	    		 driver.switchTo().window(currentwindow)	;
+	    		 Thread.sleep(4000);
+	    		 if("Welcome to CTS".equals(driver.getTitle()))
+	    		 {
+	    			 driver.findElement(By.xpath("//span[@title='Work from office']")).click();
+	    		 }
+	    		 else
+	    		 {
+	    			 driver.close();
+	    		 }
+	    	 }
+	     }
 
 	}
 
