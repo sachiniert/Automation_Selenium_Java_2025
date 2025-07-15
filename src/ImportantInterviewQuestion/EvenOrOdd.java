abe51849b0549824fa0e2904c0af0d8422795661
sachiniert <sachin.yadav.sdet@gmail.com>
2025-07-15 17:43:20 +0530

    Even or odd concept here

---
+++ src/ImportantInterviewQuestion/EvenOrOdd.java
@@ -0,0 +1,22 @@
+package ImportantInterviewQuestion;
+
+public class EvenOrOdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int number = 8;
+		 if(number % 2 == 0)
+		 {
+			 
+			 System.out.println(number + " is Even");
+		 }
+		 else
+		 {
+			 
+			 System.out.println(number + " is odd");
+		 }
+
+	}
+
+}
