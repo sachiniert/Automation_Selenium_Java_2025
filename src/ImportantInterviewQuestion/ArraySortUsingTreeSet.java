a7ce49442b29e6f2889e65a0d967282d475ef8ba
sachiniert <sachin.yadav.sdet@gmail.com>
2025-07-15 17:15:39 +0530

    Arraye Asending and Desending order

--- src/ImportantInterviewQuestion/ArraySortUsingTreeSet.java
+++ src/ImportantInterviewQuestion/ArraySortUsingTreeSet.java
@@ -1,10 +1,23 @@
 package ImportantInterviewQuestion;
+import java.util.*;
 
 public class ArraySortUsingTreeSet {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//Sample Array
+		Integer[] numbers = {5,2,8,1,3,9,4};
+		
+		// TreeSet store elements in natural(ascending ) order
+		TreeSet<Integer> ascendingSet = new TreeSet<>();
+		ascendingSet.addAll(Arrays.asList(numbers));
+		
+		System.out.println("Ascending order (TreeSet):"+ascendingSet);
+		
+		// TreeSet with reverseOrder comparator for descending
+		
+		 TreeSet<Integer> descendingSet = new TreeSet<>(Collections.reverseOrder());
+	     descendingSet.addAll(Arrays.asList(numbers));
+	     System.out.println("Descending Order (TreeSet): " + descendingSet);
 	}
 
 }
